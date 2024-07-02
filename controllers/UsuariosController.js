45e548709c59e636c9f774ac4f0040649c68ce04
vicentepaiva <vicentepaivadev@gmail.com>
2024-07-02 18:04:55 -0300

    refact userscontroller

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -1,43 +1,5 @@
 import UsuarioService from "../services/UsuarioService.js";
 
-
-// Funções de CRUD
-// function findAll() {
-//     return usuarios;
-// }
-
-// function findOne(id) {
-//     return usuarios.find(u => u.id === parseInt(id));
-// }
-
-// function create(user) {
-//     user.id = usuarios.length + 1;
-//     usuarios.push(user);
-//     return user;
-// }
-
-// function update(user, id) {
-//     const index = usuarios.findIndex(u => u.id === parseInt(id));
-//     if (index !== -1) {
-//         usuarios[index] = { ...usuarios[index], ...user };
-//         return usuarios[index];
-//     }
-//     return null;
-// }
-
-// function deleteById(id) {
-//     const index = usuarios.findIndex(u => u.id === parseInt(id));
-//     if (index !== -1) {
-//         const user = usuarios.splice(index, 1);
-//         return user[0];
-//     }
-//     return null;
-// }
-
-
-
-
-
 class UsuariosController {
     async findAll(req, res) {
         const users = await UsuarioService.findAll();

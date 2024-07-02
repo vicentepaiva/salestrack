52b311116addfe7f75a147afe369f79920b1c0eb
vicentepaiva <vicentepaivadev@gmail.com>
2024-07-01 22:10:03 -0300

    refacto salesModel

--- models/VendedoresModel.js
+++ models/VendedoresModel.js
@@ -1,46 +1,48 @@
-import Conexao from "../config/conexao";
+import Conexao from "../config/conexao.js";
 
 class VendedoresModel {
 
     async findAll() {
         const con = new Conexao();
         const sql = "SELECT * FROM vendedores";
-        const resultados = await con.query(sql, {});
+        const resultados = await con.query(sql, []);
         return resultados;
     }
 
     async findById(id) {
         const con = new Conexao();
-        const sql = "SELECT * FROM vendedores WHERE id = ?" + id;
-        const dados = {};
+        const sql = "SELECT * FROM vendedores WHERE id = $1";
+        const dados = [id];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
     async create(data){
         const con = new Conexao();
-        const sql = "INSERT INTO vendedores SET "; 
-        const dados = data;
+        const { nome, email } = data;
+        const sql = "INSERT INTO vendedores (nome, email) VALUES ($1, $2) RETURNING *";
+        const dados = [nome, email];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
     async update(data){
         const con = new Conexao();
-        const sql = "UPDATE vendedores SET ? WHERE id = ?";
-        const dados = data;
+        const { id, nome, email } = data;
+        const sql = "UPDATE vendedores SET nome = $2, email = $3 WHERE id = $1 RETURNING *";
+        const dados = [id, nome, email];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
     async delete(id){
         const con = new Conexao();
-        const sql = "DELETE FROM vendedores WHERE id = ?" + id;
-        const dados = {};
+        const sql = "DELETE FROM vendedores WHERE id = $1";
+        const dados = [id];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
-};
+}
 
 export default VendedoresModel;

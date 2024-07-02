b9e071c5934a4b1ae00f16879e3353f2e921f736
vicentepaiva <vicentepaivadev@gmail.com>
2024-07-01 22:14:22 -0300

    refactor logModel

--- models/LogModel.js
+++ models/LogModel.js
@@ -1,45 +1,49 @@
-import Conexao from "../config/conexao";
+import Conexao from "../config/conexao.js";
 
 class LogModel {
 
     async findAll() {
         const con = new Conexao();
-        const sql = "SELECT * FROM log";
-        const dados = {};
+        const sql = "SELECT * FROM logs";
+        const dados = [];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
     async findOne(id) {
         const con = new Conexao();
-        const sql = "SELECT * FROM log WHERE id = ?" + id;
-        const dados = {};
+        const sql = "SELECT * FROM logs WHERE id = $1";
+        const dados = [id];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
-    async create(data){
+    async create(data) {
         const con = new Conexao();
-        const sql = "INSERT INTO logs SET ";
-        const dados = data;
+        const { mensagem, tipo } = data;
+        const sql = "INSERT INTO logs (mensagem, tipo) VALUES ($1, $2) RETURNING *";
+        const dados = [mensagem, tipo];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
-    async update(data){
+    async update(data) {
         const con = new Conexao();
-        const sql = "UPDATE log SET mensagem = ?, tipo = ? WHERE id = ?";
-        const dados = data;
+        const { id, mensagem, tipo } = data;
+        const sql = "UPDATE logs SET mensagem = $1, tipo = $2 WHERE id = $3 RETURNING *";
+        const dados = [mensagem, tipo, id];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
-    async delete(id){
+    async delete(id) {
         const con = new Conexao();
-        const sql = "DELETE FROM log WHERE id = ?" + id;
-        const dados = {};
+        const sql = "DELETE FROM logs WHERE id = $1";
+        const dados = [id];
         const resultados = await con.query(sql, dados);
         return resultados;
     }
 
 }
+
+export default LogModel;

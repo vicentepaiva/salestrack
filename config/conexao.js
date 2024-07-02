70369b1ad09248d3969333b6d7a5c3c3d8ae84ab
vicentepaiva <vicentepaivadev@gmail.com>
2024-07-01 22:08:21 -0300

    refactor some errors with taxonomy

--- config/conexao.js
+++ config/conexao.js
@@ -3,26 +3,27 @@ import { Client } from 'pg';
 class Conexao {
   constructor() {
     this.connectionConfig = {
-        host: 'localhost',
-        user: 'admin',
-        password: 'bcf7e7d1',
-        database: 'postgres',
-        port: 5432,
-  };
-}
+      host: 'localhost',
+      user: 'admin',
+      password: 'bcf7e7d1',
+      database: 'postgres',
+      port: 5432,
+    };
+  }
 
-async qyuery(sql, params){
+  async query(sql, params) {
     const client = new Client(this.connectionConfig);
 
     try {
-        await client.connect();
-        const res = await client.query(sql, params);
-        return res.rows;
+      await client.connect();
+      const res = await client.query(sql, params);
+      return res.rows;
     } catch (error) {
-        throw error;
+      throw error;
     } finally {
-        await client.end();
+      await client.end();
     }
-    }
+  }
 }
+
 export default Conexao;
